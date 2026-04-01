91ef5859eb065d9cf152225bdd7c50abe55edce6
viniciusss500 <viniciuscarneiro500@gmail.com>
2026-03-31 21:04:46 -0300

    Update torrentService.js

--- src/services/torrentService.js
+++ src/services/torrentService.js
@@ -2,15 +2,12 @@ const { parseSize, extractEpisodeInfo } = require('../utils/parser');
 const { isBrazilianAudio } = require('../utils/filters');
 const { dedupeTorrents } = require('../utils/dedupe');
 const { getQuality } = require('../utils/quality');
-const { sortTorrents } = require('../core/scorer');
 
 function processTorrents(torrents, { season, episode }) {
   let results = torrents;
 
-  // 🇧🇷 idioma
   results = results.filter(t => isBrazilianAudio(t.name));
 
-  // 📺 séries avançado
   if (season && episode) {
     results = results.filter(t => {
       const info = extractEpisodeInfo(t.name);
@@ -20,25 +17,21 @@ function processTorrents(torrents, { season, episode }) {
         return info.season == season && info.episode == episode;
       }
 
-      // pack com múltiplos arquivos
       return info.season == season;
     });
   }
 
-  // enrich
   results = results.map(t => ({
     ...t,
     sizeBytes: parseSize(t.size),
     quality: getQuality(t.name)
   }));
 
-  // dedupe
   results = dedupeTorrents(results);
 
-  // score + ordenação
-  results = sortTorrents(results);
-
-  return results.slice(0, 20); // limite inteligente
+  return results.sort((a, b) =>
+    (b.seeders || 0) - (a.seeders || 0)
+  ).slice(0, 20);
 }
 
 module.exports = { processTorrents };
