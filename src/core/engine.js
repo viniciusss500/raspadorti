e9b86bad2dca28403471340b186d9b643c4cfef2
viniciusss500 <viniciuscarneiro500@gmail.com>
2026-03-31 21:04:33 -0300

    Update engine.js

--- src/core/engine.js
+++ src/core/engine.js
@@ -1,19 +1,9 @@
 const { searchAllIndexers } = require('../services/indexers');
 const { processTorrents } = require('../services/torrentService');
-const { log } = require('./logger');
 
 async function runSearch(params) {
-  const start = Date.now();
-
   let torrents = await searchAllIndexers(params);
-
-  log('TOTAL_ENCONTRADOS', torrents.length);
-
   torrents = processTorrents(torrents, params);
-
-  log('TOTAL_PROCESSADOS', torrents.length);
-  log('TEMPO_MS', Date.now() - start);
-
   return torrents;
 }
 
